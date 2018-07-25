214153068bb4101c754ecd083a5d0a238210ad56
Steve Geluso <stevegeluso@gmail.com>
2018-07-25 15:54:42 -0700

    init

---
+++ java/server/Application.java
@@ -0,0 +1,27 @@
+package server;
+
+import server.storage.StorageProperties;
+import server.storage.StorageService;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
+import org.springframework.context.annotation.Bean;
+
+@SpringBootApplication
+@EnableConfigurationProperties(StorageProperties.class)
+public class Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Application.class, args);
+        System.out.println("http://localhost:8080");
+    }
+
+    @Bean
+    CommandLineRunner init(StorageService storageService) {
+        return (args) -> {
+            storageService.deleteAll();
+            storageService.init();
+        };
+    }
+}
